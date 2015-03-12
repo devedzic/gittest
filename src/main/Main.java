e877f1dc5946c94c52e46dc4d0c4a5d5b002a743
Vladan <devedzic@gmail.com>
2015-03-12 22:53:39 +0100

    Main and Test classes added, with main and testApple() methods

---
+++ src/main/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Created on Mar 12, 2015
+ *
+ */
+package main;
+
+import test.Test;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Test t = new Test();
+        
+        t.testApple();
+
+    }
+
+}
---
+++ src/test/Test.java
@@ -0,0 +1,16 @@
+/*
+ * Created on Mar 12, 2015
+ *
+ */
+package test;
+
+import fruit.Apple;
+
+public class Test {
+    
+    public void testApple() {
+        Apple apple = new Apple("red", 5);
+        System.out.println(apple);
+    }
+
+}
